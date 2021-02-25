383938428889b5235e2018d3b23c447cd4a6cb2e
Kevin Masters <gmail@kevinmasters.com>
2021-02-24 20:14:24 -0500

    removed language switching jquery

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,16 +4,7 @@ jQuery(function($) {
     $('.navbar-collapse .dropdown-toggle').removeAttr('data-toggle');
     // Run the script once the document is ready
     $(document).ready(function() {
-      $('.spanish-link a').on('click', function(){
-        console.log('spanish clicked')
-        $('.spanish-link').hide();
-        $('.ingles-link').show();
-      })
-      $('.ingles-link a').on('click', function(){
-        console.log('english clicked')
-        $('.ingles-link').hide();
-        $('.spanish-link').show();
-      })
+      
     });
 
     // Run the script once the window finishes loading
